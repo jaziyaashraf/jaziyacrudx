3776034587b0f6b0331aa5a99a975d1f681928ac
jaziyaashraf <jaziyaashraf110@gmail.com>
2023-06-23 08:41:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'store';
 }
+
+// constructor (
+//   private metaService: Meta
+// ) { }
+
+// public setCanScale(canScale: boolean): void {
+//   // this.metaService.removeTag('name=viewport');
+//   if (canScale) {
+//     this.metaService.updateTag({
+//       name: 'viewport',
+//       content: 'width=device-width, initial-scale=1.0, user-scalable=yes'
+//     }, 'name=viewport');
+//   } else {
+//     this.metaService.updateTag({
+//       name: 'viewport',
+//       content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+//     }, 'name=viewport');
+//   }
+// }
+
+// }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideComponent } from './side/side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    FooterComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule
